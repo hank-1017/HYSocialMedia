294951c4173b6411390ede7efaf16f73a1839736
HY <a7892467tw99@gmail.com>
2024-12-18 16:40:09 +0800

    Changes

--- esunbank_back/src/main/java/com/hung1/esunbank_back/model/CommentRepository.java
+++
@@ -1,6 +0,0 @@
-package com.hung1.esunbank_back.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CommentRepository extends JpaRepository<Comment, Integer> {
-}
--- esunbank_back/src/main/java/com/hung1/esunbank_back/model/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.hung1.esunbank_back.model;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer> {
-
-	User findByUserName(String username);
-}
