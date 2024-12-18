7d6044498bd53b00764770c0aabb0ed81b4d0ecc
HY <a7892467tw99@gmail.com>
2024-12-18 16:43:43 +0800

    Changes

---
+++ HYSocialMedia_front/src/components/Navbar.vue
@@ -0,0 +1,71 @@
+<template xmlns="">
+
+  <nav class="navbar navbar-expand-lg bg-light">
+    <div class="container-fluid">
+
+      <router-link to="/">
+        <a class="navbar-brand">Simple-Social-Network</a>
+      </router-link>
+
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+
+          <li class="nav-item">
+            <router-link to="/">
+              <a class="nav-link active" aria-current="page">用戶資訊</a>
+            </router-link>
+          </li>
+
+          <li class="nav-item">
+            <router-link to="/post/add">
+              <a class="nav-link">新增文章</a>
+            </router-link>
+          </li>
+
+          <li class="nav-item">
+            <router-link to="/post/show">
+              <a class="nav-link">全部文章</a>
+            </router-link>
+          </li>
+
+          <li class="nav-item">
+            <router-link to="/register">
+              <a class="nav-link">註冊</a>
+            </router-link>
+          </li>
+
+          <li class="nav-item">
+            <router-link to="/login">
+              <a class="nav-link">登入</a>
+            </router-link>
+          </li>
+
+          <li class="nav-item">
+            <a class="nav-link" @click="logout" href="">登出</a>
+          </li>
+
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <div class="container">
+    <router-view/>
+  </div>
+</template>
+
+<script setup>
+import 'bootstrap/dist/css/bootstrap.min.css';
+import axios from "axios";
+
+const URL = import.meta.env.VITE_API_JAVAURL;
+const logout = async () => {
+  const response = await axios.get(`${URL}user/logout`, {withCredentials: true});
+  if (response.data === "Y") {
+    alert("已登出")
+  }
+}
+
+</script>
+
+
---
+++ HYSocialMedia_front/src/router.js
@@ -0,0 +1,48 @@
+import {createRouter, createWebHistory} from 'vue-router'
+import HomePage from '../src/views/HomePage.vue';
+import Register from '../src/views/RegisterPage.vue';
+import LoginPage from '../src/views/LoginPage.vue';
+import AddPostPage from "./views/AddPostPage.vue";
+import ShowPostPage from "./views/ShowPostPage.vue";
+import Navbar from '../src/components/Navbar.vue';
+
+
+const routes = [
+
+    {
+        path: '/',
+        component: Navbar,
+        children: [
+            {
+                path: '/',
+                component: HomePage,
+            },
+            {
+                path: 'register',
+                component: Register,
+            },
+            {
+                path: 'login',
+                component: LoginPage,
+            },
+            {
+                path: 'post/add',
+                component: AddPostPage,
+            },
+            {
+                path: 'post/show',
+                component: ShowPostPage,
+            },
+
+        ]
+    },
+]
+
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+})
+
+
+export default router
